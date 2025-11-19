7c4708be8f19ad3d2a26d93b0816f6a934c03edd
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2025-11-19 23:32:05 +0100

    Possible way to combine score and weight

--- app/__init__.py
+++ app/__init__.py
@@ -101,6 +101,25 @@ class Category:
 
     @staticmethod
     def parameter_score(parameter: Tuple[Parameter, float]):
+        """Possible formula: Outputs a float in range [0, 1]\n
+            Weight:
+                If weight = 1 then score will not be weighted
+                As weight -> 0: final_score -> 1 i.e. score matters less
+                As weight -> inf: final_score -> 0 i.e. score matters more
+            Score:
+                If score = 1 then final_score = 1\n
+                If score = 0 then final_score = 0 
+            Drawbacks:
+                \"Anomalies\" when score is very close to 1\n
+                If score = 0.99 then weight needs to be around 25 for final_score to be (almost) 0\n
+                If score = 0.9999 then weight needs to be around 250 for final_score to be (almost) 0
+            Visual:
+            https://www.desmos.com/calculator/j0krldwn5u
+        """
+        score = parameter[0].calculate_score()
+        weight = parameter[1]
+
+        return score ** (weight ** 2)
         """TODO: Change the formula"""
         parameter[0].calculate_score() * parameter[1]
 
