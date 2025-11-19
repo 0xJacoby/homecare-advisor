8bd37af0e5300bd45235ef15f02131211c3e04fc
Folke Ishii <ishii@student.chalmers.se>
2025-11-19 17:02:04 +0100

    Implement schema in database

--- app/models/patient.py
+++ app/models/patient.py
@@ -5,8 +5,10 @@ class Patient(db.Model):
     __tablename__ = "patients"
 
     ssn = db.Column(db.String(13), primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    surname = db.Column(db.String(100), nullable=False)
+    firstname = db.Column(db.Text, nullable=False)
+    surname = db.Column(db.Text, nullable=False)
+    date_of_birth = db.Column(db.DateTime(timezone=True), nullable=False)
+    municipality = db.Column(db.Text, nullable=False)
 
     journal_entries = db.relationship(
         "JournalEntry",
@@ -14,16 +16,20 @@ class Patient(db.Model):
         lazy=True
     )
 
-    def __init__(self, ssn, firstname="", surname=""):
+    def __init__(self, ssn, firstname, surname, date_of_birth, municipality):
         self.ssn = ssn
         self.firstname = firstname
         self.surname = surname
+        self.date_of_birth = date_of_birth
+        self.municipality = municipality
 
     def to_dict(self):
         return {
             "ssn": self.ssn,
+            "date_of_birth": self.date_of_birth,
             "firstname": self.firstname,
-            "surname": self.surname
+            "surname": self.surname,
+            "municipality": self.municipality,
         }
 
     @staticmethod
